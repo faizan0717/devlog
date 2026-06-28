4af7a8e9b8a544f7797fc5f56e3490f50b1e2031
faizan ahmed <faizanahmadi2000@gmail.com>
2026-06-28 17:27:09 +0530

    analytics for vercel

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './styles/globals.css'
 
@@ -9,5 +10,6 @@ if (!root) throw new Error('Root element not found')
 createRoot(root).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
